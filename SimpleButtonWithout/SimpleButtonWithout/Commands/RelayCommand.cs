c9ff681bb03e5b676d18cae39fc934e39492aae8
ko9 <ko9@DESKTOP-ESG79SD>
2016-10-17 15:25:17 +0300

    initial upload

---
+++ SimpleButtonWithout/SimpleButtonWithout/Commands/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleButtonWithout.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        Action execute;
+        Func<bool> canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
---
+++ SimpleButtonWithout/SimpleButtonWithout/ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using SimpleButtonWithout.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleButtonWithout.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        private String output;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnChange(string propertyname)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+        public bool IsClickable { get; set; }
+
+        public RelayCommand OnButtonClickedCommand { get; set; }
+
+        public String Output
+        {
+            get { return output; }
+            set { output = value; OnChange("Output"); }
+        }
+
+        public MainViewModel()
+        {
+            OnButtonClickedCommand = new RelayCommand(
+                new Action(DoItOnButtonClick), 
+                new Func<bool>(CanExecuteButton));
+        }
+
+        private bool CanExecuteButton()
+        {
+            return IsClickable;
+        }
+
+        private void DoItOnButtonClick()
+        {
+            Output += "clicked!";
+        }
+    }
+}

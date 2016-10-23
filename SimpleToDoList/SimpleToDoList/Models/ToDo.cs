e93ac6d3e3d4d6f182a6c917771e552c01d2d0de
ko9 <ko9@DESKTOP-ESG79SD>
2016-10-23 14:36:06 +0300

    updates

---
+++ SimpleToDoList/SimpleToDoList/Models/ToDo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToDoList.Models
+{
+    public class ToDo
+    {
+
+
+        public String Description { get; set; }
+        public Boolean Done { get; set; }
+
+        public ToDo(string description, bool done)
+        {
+            Description = description;
+            Done = done;
+        }
+
+
+
+    }
+}
---
+++ SimpleToDoList/SimpleToDoList/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToDoList.ViewModels
+{
+    class MainViewModel
+    {
+        private string newTaskDescription;
+
+        public string NewTaskDescription
+        {
+            get { return newTaskDescription; }
+            set { newTaskDescription = value; }
+        }
+
+        private ObservableCollection<String> toDoList = new ObservableCollection<string>();
+
+        public ObservableCollection<String> ToDoList
+        {
+            get { return toDoList; }
+            set { toDoList = value; }
+        }
+
+        public MainViewModel()
+        {
+            ToDoList.Add("sdfsdf");
+            ToDoList.Add("sdfsdf");
+        }
+
+    }
+}

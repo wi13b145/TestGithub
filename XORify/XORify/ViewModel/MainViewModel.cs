022a5959206138bd20c90c599a9f48a3af652e60
ko9 <ko9@DESKTOP-ESG79SD>
2016-10-23 22:21:32 +0300

    1st wireframe done

---
+++ XORify/XORify/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XORify.ViewModel
+{
+    class MainViewModel
+    {
+        private string inputTxt;
+
+        public string InputTxt
+        {
+            get { return inputTxt; }
+            set { inputTxt = value; }
+        }
+
+        private string keyTxt;
+
+        public string KeyTxt
+        {
+            get { return keyTxt; }
+            set { keyTxt = value; }
+        }
+
+        private string outputTxt;
+
+        public string OutputTxt
+        {
+            get { return outputTxt; }
+            set { outputTxt = value; }
+        }
+
+
+        public MainViewModel()
+        {
+           
+        }
+
+    }
+}

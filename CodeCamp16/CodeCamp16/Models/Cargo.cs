949a40a39a9d22fca08c140e4945389b5009254f
ko9 <ko9@DESKTOP-ESG79SD>
2016-11-04 14:28:19 +0100

    MVVMlight installed

---
+++ CodeCamp16/CodeCamp16/Models/Cargo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCamp16.Models
+{
+    public class Cargo
+    {
+
+        #region PROPERTIES
+
+        public String ItemName { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Amount { get; set; }
+
+        #endregion
+
+
+
+        public Cargo(string itemName, int weight, int amount)
+        {
+            ItemName = itemName;
+            Weight = weight;
+            Amount = amount;
+        }
+
+
+
+
+    }
+}
---
+++ CodeCamp16/CodeCamp16/Models/Truckload.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCamp16.Models
+{
+    public class Truckload
+    {
+
+
+        public String Source { get; set; }
+        public int TimeToReady { get; set; }
+
+        public List<Cargo> Load { get; set; }
+
+
+        public Truckload(string source, int timeToReady, List<Cargo> load)
+        {
+            Source = source;
+            TimeToReady = timeToReady;
+            Load = load;
+        }
+
+
+    }
+}
